437a0b8ae31ef442628b5628daca0471ba1a68a3
FahrezRB <fahreza.raditya.busan@gmail.com>
2026-01-29 19:43:25 +0700

    fix: Increment patch version
    Increments the `apiVersionPatch` from 110 to 200 in `manifest.gradle.kts`.

--- manifest.gradle.kts
+++ manifest.gradle.kts
@@ -1,6 +1,6 @@
 val apiVersionMajor = 1
 val apiVersionMinor = 4
-val apiVersionPatch = 110
+val apiVersionPatch = 200
 
 val patchDigits = apiVersionPatch.toString().length
 val patchShift = when (patchDigits) {
